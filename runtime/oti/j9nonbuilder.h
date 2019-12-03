071719f63f94efe59f950a61c97a0488acc17155
Sharon Wang <sharon-wang-cpsc@outlook.com>
2019-12-03 11:17:47 -0500

    Leverage SCC for Class Relationship Verification
    Record and process class relationship snippets, which indicate the
    indices of the source and target classes of a relationship in the
    classNameList of verifyData.
    
    Store class relationship snippets for a romClass into the shared
    class cache during the first run of an application. Snippets are
    stored for classes that have passed bytecode verification. On
    subsequent runs of the application, snippets will already exist in
    the cache for the romClass, which means that the class's bytecodes
    had already been verified and thus we can skip the linear bytecode
    walk.
    
    This work is an extension of the existing
    -XX:+ClassRelationshipVerifier work. It is enabled/disabled using
    the same command-line options.
    
    Signed-off-by: Sharon Wang <sharon-wang-cpsc@outlook.com>

--- runtime/oti/j9nonbuilder.h
+++ runtime/oti/j9nonbuilder.h
@@ -1847,6 +1847,7 @@ typedef struct J9BytecodeVerificationData {
 	struct J9PortLibrary * portLib;
 	struct J9JavaVM* javaVM;
 	BOOLEAN createdStackMap;
+	struct J9HashTable* classRelationshipSnippetsHashTable;
 } J9BytecodeVerificationData;
 
 /* @ddr_namespace: map_to_type=J9NativeLibrary */
